3d562e748d48e59ec9e87925ad940f4aba303669
danikmartyniuk <daniilmartyniuk.sbmt@gmail.com>
2020-07-30 18:49:50 +0300

    Made a design of 'Today' activity

--- app/src/main/java/com/example/weatherapp/TodayActivity.kt
+++ app/src/main/java/com/example/weatherapp/TodayActivity.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TodayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
